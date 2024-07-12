3ad4bf37234488f4c8bb374c812ae2763278bd5b
aginCoder <phamgiaan545@gmail.com>
2024-07-12 19:24:54 +0700

    Create lesson-02-enum-ordinal

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -7,12 +7,14 @@ public class Program {
             factory.inTransaction(session -> {
                 var department = new Department();
                 department.setName("Giám đốc");
+                department.setType(Department.Type.PROJECT_MANAGER);
                 session.persist(department);
             });
 
-            factory.inSession(session -> {
+            factory.inTransaction(session -> {
                 var department = new Department();
                 department.setName("Bảo vệ");
+                department.setType(Department.Type.TESTER);
                 session.persist(department);
             });
 
@@ -25,31 +27,6 @@ public class Program {
                 for (var department :deppartments){
                     System.out.println("department = " + department);
                 }
-            });
-
-            factory.inSession(session -> {
-                var department = session.get(Department.class, 1);
-                System.out.println("department = " + department);
-            });
-
-            factory.inSession(session -> {
-                var hql = "FROM Department WHERE name = :name";
-                var department = session
-                        .createSelectionQuery(hql, Department.class)
-                        .setParameter("name", "Bảo vệ")
-                        .uniqueResult();
-                System.out.println("department = " + department);
-            });
-
-            factory.inTransaction(session -> {
-                var department = session.get(Department.class, 2);
-                department.setName("Kinh doanh");
-                session.merge(department);
-            });
-
-            factory.inTransaction(session -> {
-                var department = session.get(Department.class, 1);
-                session.remove(department);
             });
 
         }
