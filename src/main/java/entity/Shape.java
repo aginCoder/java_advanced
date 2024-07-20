6c37b94eb36bac3ba5b6303c11af5ec8ef6f2667
aginCoder <phamgiaan545@gmail.com>
2024-07-20 11:55:53 +0700

    create

--- src/main/java/entity/Shape.java
+++ src/main/java/entity/Shape.java
@@ -1,17 +1,25 @@
 package entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@MappedSuperclass
+@Entity
+@Table(name = "shape")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Shape {
     @Id
     @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name = "color", length = 50, nullable = false)
